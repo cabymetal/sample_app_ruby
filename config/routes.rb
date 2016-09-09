6044d8859bacc1256b3533fe75da29b5232246aa
Carlos Murillo <cabymetal@gmail.com>
2016-09-09 22:27:02 +0000

    Finished sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,12 @@ Rails.application.routes.draw do
   get '/about',       to:'static_pages#about'
   get '/signup',      to:'users#new'
   get '/contact',     to:'static_pages#contact'
-
-  #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  post '/signup',  to: 'users#create'
+  
+  resources :users
+  
+  #Fordetails on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root "static_pages#home"
 end
