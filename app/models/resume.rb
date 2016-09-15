0ece09d33cce86524659334f415ada2aa8808dcd
Carlos Murillo <cabymetal@gmail.com>
2016-09-15 02:44:48 +0000

    Finished AJAX request, form returned to index

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -4,4 +4,12 @@ class Resume < ApplicationRecord
   validates :user_id, presence: true
   validates :file_url, presence: true
   mount_uploader :file_url, AttachmentUploader
+  
+  def self.search(search)
+    if search
+      where('file_url LIKE ?', "%#{search}%")
+    else
+      all
+    end
+  end
 end
